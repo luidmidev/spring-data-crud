3c89f42caa711498d0713898ec9bd549c774aed7
LUIDMIDEV <88100292+luidmidev@users.noreply.github.com>
2025-01-01 17:07:42 -0500

    .

--- spring-data-crud-jpa/src/main/java/io/github/luidmidev/springframework/data/crud/jpa/services/JpaCrudService.java
+++ spring-data-crud-jpa/src/main/java/io/github/luidmidev/springframework/data/crud/jpa/services/JpaCrudService.java
@@ -4,6 +4,7 @@ package io.github.luidmidev.springframework.data.crud.jpa.services;
 import io.github.luidmidev.springframework.data.crud.core.services.CrudService;
 import io.github.luidmidev.springframework.data.crud.jpa.utils.AdvanceSearch;
 import jakarta.persistence.EntityManager;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Persistable;
@@ -18,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
  * @param <ID> ID
  * @param <R>  Repositorys
  */
-@Transactional
 public abstract class JpaCrudService<M extends Persistable<ID>, D, ID, R extends JpaRepository<M, ID>> extends CrudService<M, D, ID, R> {
 
     protected final EntityManager entityManager;
@@ -26,6 +26,18 @@ public abstract class JpaCrudService<M extends Persistable<ID>, D, ID, R extends
     protected JpaCrudService(R repository, Class<M> domainClass, EntityManager entityManager) {
         super(repository, domainClass);
         this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public M create(@NotNull D dto) {
+        return super.create(dto);
+    }
+
+    @Override
+    @Transactional
+    public M update(@NotNull ID id, @NotNull D dto) {
+        return super.update(id, dto);
     }
 
     @Override
